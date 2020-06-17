68b6db554522b64dacf7b7a673ac124bd84a93b9
Vadim8576 <vr-project@yandex.ru>
2020-06-17 17:42:41 +0300

    test

--- src/utils/genres_reducer_helper.js
+++ src/utils/genres_reducer_helper.js
@@ -3,3 +3,6 @@ export const updateObjectInArray = (items, itemId) => {
     const id = items.length > 0 ? items.indexOf(itemId) : -1;
     return id === -1 ? [...items, itemId] : items.filter(g => g !== itemId);
 }
+
+
+
